85496389c722f25110d346a603020c5d354381fb
Trian Damai <triannurizkillah@gmail.com>
2020-02-17 03:18:57 +0700

    add auth page

--- app/src/main/java/com/pmo/iderin/Auth.java
+++ app/src/main/java/com/pmo/iderin/Auth.java
@@ -1,14 +1,46 @@
 package com.pmo.iderin;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class Auth extends AppCompatActivity {
+    private Context context = Auth.this;
+
+    @BindView(R.id.et_email)
+    EditText etEmail;
+    @BindView(R.id.et_password)
+    EditText etPassword;
+    @BindView(R.id.btn_masuk)
+    Button btnMasuk;
+    @BindView(R.id.tv_toregister)
+    TextView tvToregister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btn_masuk, R.id.tv_toregister})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_masuk:
+                startActivity(new Intent(context,MainActivity.class));
+                break;
+            case R.id.tv_toregister:
+                break;
+        }
     }
 }
--- app/src/main/java/com/pmo/iderin/Splash.java
+++ app/src/main/java/com/pmo/iderin/Splash.java
@@ -3,6 +3,7 @@ package com.pmo.iderin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -31,7 +32,8 @@ public class Splash extends AppCompatActivity {
             new Handler().postDelayed(new Runnable() {
                 @Override
                 public void run() {
-
+                    startActivity(new Intent(context,Auth.class));
+                    finish();
                 }
             }, 200);
         }
