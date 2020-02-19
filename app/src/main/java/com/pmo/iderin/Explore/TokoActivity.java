4defa362000ab6f2f511d8416f4ffa0a6f85c266
Trian Damai <triannurizkillah@gmail.com>
2020-02-19 07:21:33 +0700

    doing front end

---
+++ app/src/main/java/com/pmo/iderin/Explore/TokoActivity.java
@@ -0,0 +1,16 @@
+package com.pmo.iderin.Explore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pmo.iderin.R;
+
+public class TokoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toko);
+    }
+}
---
+++ app/src/main/java/com/pmo/iderin/Profile/TokoSayaActivity.java
@@ -0,0 +1,16 @@
+package com.pmo.iderin.Profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pmo.iderin.R;
+
+public class TokoSayaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toko_saya);
+    }
+}
