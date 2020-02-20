a9ad4a64e51d85d45d86c6198a9dc3d0f2961bb6
Trian Damai <triannurizkillah@gmail.com>
2020-02-20 15:28:57 +0700

    upload profil

--- app/src/main/java/com/pmo/iderin/Profile/EditProfilActivity.java
+++ app/src/main/java/com/pmo/iderin/Profile/EditProfilActivity.java
@@ -1,19 +1,37 @@
 package com.pmo.iderin.Profile;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.pmo.iderin.R;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,6 +41,7 @@ import static com.pmo.iderin.Helpers.windowManager.getTranparentStatusBar;
 
 public class EditProfilActivity extends AppCompatActivity {
 
+    private static final int PICK_IMAGE_REQUEST = 23;
     @BindView(R.id.iv_fotoprofil)
     ImageView ivFotoprofil;
     @BindView(R.id.et_nama)
@@ -39,6 +58,9 @@ public class EditProfilActivity extends AppCompatActivity {
     private DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
     private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
     private FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+    private FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
+    private StorageReference storageReference = firebaseStorage.getReference();
+    private Uri filePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,12 +70,88 @@ public class EditProfilActivity extends AppCompatActivity {
         getTranparentStatusBar(this);
     }
 
+    private void selectImage(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,
+                "select image from here.."),
+                PICK_IMAGE_REQUEST);
+    }
+
+    private void upload(){
+        if(filePath != null){
+            ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading");
+            progressDialog.show();
+
+            StorageReference myref = storageReference
+                    .child("")
+                    .child("images"+firebaseAuth.getCurrentUser().getUid());
+            ivFotoprofil.setDrawingCacheEnabled(true);
+            ivFotoprofil.buildDrawingCache();
+
+            Bitmap bitmap = ((BitmapDrawable) ivFotoprofil.getDrawable()).getBitmap();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG,80,baos);
+            byte[] data = baos.toByteArray();
+
+            UploadTask uploadTask = myref.putBytes(data);
+            Task<Uri> uriTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                @Override
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                    if(!task.isSuccessful()){
+                        throw task.getException();
+                    }
+                    return  myref.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                      if(task.isSuccessful()){
+                          Uri donloadUri = task.getResult();
+
+                          //aksi tambah ke db
+                      }else {
+                          //gagal
+                      }
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //cek request
+        if(requestCode == PICK_IMAGE_REQUEST &&
+                resultCode == RESULT_OK &&
+                data != null &&
+                data.getData() != null){
+                filePath = data.getData();
+                try {
+                    Bitmap bitmap = MediaStore
+                            .Images
+                            .Media
+                            .getBitmap(getContentResolver(),filePath);
+                    ivFotoprofil.setImageBitmap(bitmap);
+
+                }catch (IOException io){
+                    io.printStackTrace();
+                }
+
+        }
+    }
+
     @OnClick({R.id.iv_fotoprofil, R.id.btn_simpan})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.iv_fotoprofil:
+                selectImage();
                 break;
             case R.id.btn_simpan:
+                upload();
                 break;
         }
     }
