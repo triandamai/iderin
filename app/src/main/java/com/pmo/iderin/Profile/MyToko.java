9abfee30addb90477bc2d0cee041b512548d96a2
Trian Damai <triannurizkillah@gmail.com>
2020-02-22 10:55:49 +0700

    major update 2

--- app/src/main/java/com/pmo/iderin/Profile/MyToko.java
+++ app/src/main/java/com/pmo/iderin/Profile/MyToko.java
@@ -1,16 +1,54 @@
 package com.pmo.iderin.Profile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.pmo.iderin.R;
 
 public class MyToko extends AppCompatActivity {
+    private DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    private FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+    private Context context = MyToko.this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_toko);
     }
+
+    private void getBarangToko() {
+        databaseReference
+                .child(getResources().getString(R.string.CHILD_BARANG))
+                .orderByChild("")
+                .startAt("")
+                .endAt("").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    try {
+
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
 }
